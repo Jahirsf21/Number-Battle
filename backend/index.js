4a61cb954e0eae705a221a31f79a1877b1f2273e
Jahirsf21 <deislhersanchezfunez@gmail.com>
2025-08-24 20:22:14 -0600

    Backend: Funcion realizarIntento, encargada de la jugabilidad cambios de turno, adivinar el número secreto y aumento de rondas

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,11 @@ const archivoPartidas = path.join(__dirname, "partidas.json");
 
 let partidas = obtenerPartidas();
 
+
+// Método para obtener las partidas de un json
+// En caso de no existir el archivo retorna un array vacio.
+// Si el archivo existe pero esta vacio retorna un array vacio.
+// Si el archivo existe y tiene información retorna los datos parseados.
 function obtenerPartidas() {
   if (!fs.existsSync(archivoPartidas)) return [];
   try {
@@ -21,6 +26,8 @@ function obtenerPartidas() {
   }
 }
 
+
+// Método para guardar las partidas en un json
 function guardarPartidas() {
   try {
     fs.writeFileSync(ArchivoPartidas, JSON.stringify(partidas, null, 2));
@@ -32,7 +39,9 @@ function guardarPartidas() {
 }
 
 
-
+// Método POST
+// recibe el nombre del jugador 1 y del jugador
+// retorna los datos correspondientes a la partida creada y guarda la partida en el json
 app.post("/crearPartida", (req, res) => {
   const { nombrePrimerJugador, nombreSegundoJugador } = req.body;
 
@@ -40,9 +49,9 @@ app.post("/crearPartida", (req, res) => {
     return res.status(400).json({ error: "Los nombres de los jugadores no pueden estar vacíos" });
   }
   
-  let jugadores = [nombrePrimerJugador, nombreSegundoJugador];
-  jugadores = jugadores.sort(() => Math.random() - 0.5);
-  const turnoInicial = jugadores[0];
+  let jugadores = [nombrePrimerJugador, nombreSegundoJugador]; 
+  jugadores = jugadores.sort(() => Math.random() - 0.5); //se cambia el orden de los jugadores de forma random
+  const turnoInicial = jugadores[0]; //el jugador en la primera posición sera el que empieza el juego
 
   const partida = {
     id: partidas.length +1,
@@ -82,6 +91,72 @@ app.post("/crearPartida", (req, res) => {
   res.json(partida);
 });
 
+
+
+// Método POST, recibe el id de la partida que se esta jugando, el jugador actual y el número ingresado para adivinar.
+// Si el id no existe en las partidas guardadas retorna un mensaje de error 
+// retorna la partida actualiza, el mensaje y su tipo (se estan usando sweetAlerts)
+app.post("/realizarIntento", (req, res) => {
+  const { partidaId, jugador, intento } = req.body;
+  const partida = partidas.find(p => p.id === partidaId);
+
+  if (!partida) return res.status(404).json({ error: "Partida no encontrada" });
+
+  const jugadorObj = partida.informacionJugadores[jugador];
+  const oponenteNombre = partida.jugador1 === jugador ? partida.jugador2 : partida.jugador1;
+  const oponenteObj = partida.informacionJugadores[oponenteNombre];
+
+  const ronda = jugadorObj.rondas[jugadorObj.rondaActual];
+  const guess = parseInt(intento);
+
+  let mensaje = "";
+  let tipo = "info";
+
+  ronda.intentosUsados++;
+  jugadorObj.totalIntentos++;
+
+  if (guess === ronda.numeroSecreto) {
+    ronda.adivinado = true;
+    mensaje = `¡Correcto! Adivinaste el número en la ronda ${jugadorObj.rondaActual + 1}.`;
+    tipo = "success";
+    ronda.duracion = Math.floor((Date.now() - partida.timestampTurnoInicio) / 1000);
+    jugadorObj.rondaActual++; 
+    const jugadorActualTermino = jugadorObj.rondaActual === partida.rondasTotales;
+    const oponenteTermino = oponenteObj.rondaActual === partida.rondasTotales;
+
+    if (jugadorActualTermino && !oponenteTermino) {
+ 
+      partida.turnoActual = oponenteNombre;
+      partida.timestampTurnoInicio = Date.now();
+      mensaje += ` Turno de ${oponenteNombre}.`;
+
+    } else if (jugadorActualTermino && oponenteTermino) {
+      partida.juegoTerminado = true;
+      if (jugadorObj.totalIntentos < oponenteObj.totalIntentos) {
+        partida.ganadorPartida = jugador;
+      } else if (oponenteObj.totalIntentos < jugadorObj.totalIntentos) {
+        partida.ganadorPartida = oponenteNombre;
+      } else {
+        partida.ganadorPartida = "Empate";
+      }
+       mensaje += " ¡Juego terminado!";
+    } else {
+      
+       partida.timestampTurnoInicio = Date.now();
+    }
+
+  } else {
+    mensaje = guess < ronda.numeroSecreto ? "El número secreto es MÁS ALTO." : "El número secreto es MÁS BAJO.";
+  }
+
+  guardarPartidas();
+
+  res.json({
+    mensaje,
+    tipo,
+    partidaActualizada: partida
+  });
+});
 
 app.listen(3000, () => {
   console.log("Servidor escuchando en el puerto 3000");
