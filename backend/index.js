7296201947949913c88ba6a9fa716a7f3461b3fb
Jahirsf21 <deislhersanchezfunez@gmail.com>
2025-08-24 20:10:48 -0600

    Backend: Función para obtener y guardar partidas en un json. ajustes a la función crearPartida para que funcionen las 3 rondas con distintos números secretos

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,37 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const path = require("path");
+
 app.use(cors());
 app.use(express.json());
+
+const archivoPartidas = path.join(__dirname, "partidas.json");
+
+let partidas = obtenerPartidas();
+
+function obtenerPartidas() {
+  if (!fs.existsSync(archivoPartidas)) return [];
+  try {
+    const data = fs.readFileSync(archivoPartidas, "utf-8");
+    return data ? JSON.parse(data) : [];
+  } catch (error) {
+    console.error("Error al leer o parsear partidas.json:", error);
+    return [];
+  }
+}
+
+function guardarPartidas() {
+  try {
+    fs.writeFileSync(ArchivoPartidas, JSON.stringify(partidas, null, 2));
+    console.log("Partidas guardadas en el archivo.");
+  } catch (error)
+  {
+    console.error("Error al guardar las partidas:", error);
+  }
+}
+
+
 
 app.post("/crearPartida", (req, res) => {
   const { nombrePrimerJugador, nombreSegundoJugador } = req.body;
@@ -14,28 +43,42 @@ app.post("/crearPartida", (req, res) => {
   let jugadores = [nombrePrimerJugador, nombreSegundoJugador];
   jugadores = jugadores.sort(() => Math.random() - 0.5);
   const turnoInicial = jugadores[0];
+
   const partida = {
+    id: partidas.length +1,
     jugador1: jugadores[0],
     jugador2: jugadores[1],
-    rondasTotales: 6,
-    rondaActual: 1,
-    tiempoInicio: null,
-    tiempoTotal: null,
+    rondasTotales: 3,
+    turnoActual: turnoInicial,
+    juegoTerminado: false,
+    ganadorPartida: null,
+    timestampTurnoInicio: Date.now(),
     informacionJugadores: {
       [jugadores[0]]: {
-        intentos: 3,
-        numeroSecreto: Math.floor(Math.random() * 100) + 1,
-        duracion: null
+        rondas: Array.from({ length: 3 }, () => ({
+          numeroSecreto: Math.floor(Math.random() * 100) + 1,
+          intentosUsados: 0,
+          adivinado: false,
+          duracion: 0
+        })),
+        rondaActual: 0,
+        totalIntentos: 0
       },
       [jugadores[1]]: {
-        intentos: 3,
-        numeroSecreto: Math.floor(Math.random() * 100) + 1,
-        duracion: null
+        rondas: Array.from({ length: 3 }, () => ({
+          numeroSecreto: Math.floor(Math.random() * 100) + 1,
+          intentosUsados: 0,
+          adivinado: false,
+          duracion: 0
+        })),
+        rondaActual: 0,
+        totalIntentos: 0
       }
-    },
-    turnoInicial
+    }
   };
 
+  partidas.push(partida);
+  guardarPartidas();
   res.json(partida);
 });
 
